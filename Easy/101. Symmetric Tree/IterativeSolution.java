9a6c8cfe3b442c2a4c58180c0bee5e380cef0c46
Ihor Nikora <ihor.nikora@gmail.com>
2021-01-27 00:40:53 +0200

    101. Symmetric Tree: Java - Iterative

---
+++ Easy/101. Symmetric Tree/IterativeSolution.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class IterativeSolution {
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode node1 = q.poll();
+            TreeNode node2 = q.poll();
+
+            if (node1 == null && node2 == null)
+                continue;
+
+            if (node1 == null || node2 == null)
+                return false;
+
+            if (node1.val != node2.val)
+                return false;
+
+            q.add(node1.left);
+            q.add(node2.right);
+            q.add(node1.right);
+            q.add(node2.left);
+        }
+        return true;
+    }
+}
