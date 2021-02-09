2b689c2dbbd908fecbfbd6468e837c950b94459b
Ihor Nikora <ihor.nikora@gmail.com>
2021-02-09 23:38:08 +0200

    1038. Binary Search Tree to Greater Sum Tree: Java

---
+++ Medium/1038. Binary Search Tree to Greater Sum Tree/Solution.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int acc = 0;
+
+    public TreeNode bstToGst(TreeNode root) {
+        if (root == null)
+            return null;
+
+        bstToGst(root.right);
+
+        acc += root.val;
+        root.val = acc;
+
+        bstToGst(root.left);
+
+        return root;
+    }
+}
