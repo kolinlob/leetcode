af22595d6320ec5d72b1998b4bb183f7bd757ede
Ihor Nikora <ihor.nikora@gmail.com>
2021-02-17 23:03:58 +0200

    1290. Convert Binary Number in a Linked List to Integer: Java

---
+++ Easy/1290. Convert Binary Number in a Linked List to Integer/Solution.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        ListNode cur = head;
+
+        int res = 0;
+        while (cur != null) {
+            res = res << 1 | cur.val;
+            cur = cur.next;
+        }
+
+        return res;
+    }
+}
