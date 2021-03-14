0228bffc90fcfb4b1fe496d098d17ae978234f7f
Ihor Nikora <ihor.nikora@gmail.com>
2021-03-14 12:14:29 +0200

    823. Binary Trees With Factors: Java

--- Medium/823. Binary Trees With Factors/Solution.java
+++ Medium/823. Binary Trees With Factors/Solution.java
@@ -8,18 +8,19 @@ class Solution {
         long[] dp = new long[n];
         Arrays.fill(dp, 1);
 
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap();
         for (int i = 0; i < n; i++)
             map.put(arr[i], i);
 
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 if (arr[i] % arr[j] == 0) {
-                    int right = arr[i] / arr[j];
-                    if (map.containsKey(right))
-                        dp[i] = (dp[i] + dp[j] * dp[map.get(right)]) % mod;
+                    int other = arr[i] / arr[j];
+                    if (map.containsKey(other))
+                        dp[i] += (dp[j] * dp[map.get(other)]) % mod;
                 }
             }
+        }
 
         long res = 0;
         for (long x : dp)
