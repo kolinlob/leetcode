1c6b5f2852fb626a1bceee8ad57c93889e9e14a5
Ihor Nikora <ihor.nikora@gmail.com>
2021-03-27 17:09:56 +0200

    647. Palindromic Substrings: Java

--- Medium/647. Palindromic Substrings/Solution.java
+++ Medium/647. Palindromic Substrings/Solution.java
@@ -1,24 +1,22 @@
 class Solution {
     public int countSubstrings(String s) {
-        int n = s.length();
-        boolean[][] dp = new boolean[n][n];
-        int count = n;
+        int count = 0;
 
-        for (int i = 0; i < n; i++)
-            dp[i][i] = true;
-
-        for (int i = 0; i < n - 1; i++) {
-            dp[i][i + 1] = s.charAt(i) == s.charAt(i + 1);
-            count += dp[i][i + 1] ? 1 : 0;
-        }
-
-        for (int len = 3; len <= n; len++) {
-            for (int i = 0, j = len + i - 1; j < n; i++, j++) {
-                dp[i][j] = dp[i + 1][j - 1] && s.charAt(i) == s.charAt(j);
-                count += dp[i][j] ? 1 : 0;
-            }
+        for (int i = 0; i < s.length(); i++) {
+            count += countPalindromes(s, i, i);
+            count += countPalindromes(s, i, i + 1);
         }
 
         return count;
+    }
+
+    private int countPalindromes(String s, int i, int j) {
+        int res = 0;
+        while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) {
+            res += 1;
+            i -= 1;
+            j += 1;
+        }
+        return res;
     }
 }
