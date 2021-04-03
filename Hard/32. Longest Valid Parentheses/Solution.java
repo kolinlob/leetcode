e0f5bda59fe6affea70269f22dbc651bcd0bfe34
Ihor Nikora <ihor.nikora@gmail.com>
2021-04-04 00:22:27 +0300

    32. Longest Valid Parentheses: Java

---
+++ Hard/32. Longest Valid Parentheses/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int max = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(')
+                stack.push(i);
+            else {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                    if (!stack.isEmpty())
+                        max = Math.max(max, i - stack.peek());
+                    else
+                        stack.push(i);
+                }
+            }
+        }
+
+        return max;
+    }
+}
