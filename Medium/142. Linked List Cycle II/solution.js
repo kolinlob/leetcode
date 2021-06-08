dae7a9d074de6e453b622787a5dc0bb1b89d5329
Ihor Nikora <ihor.nikora@gmail.com>
2021-06-08 22:44:49 +0300

    142. Linked List Cycle II: JS

---
+++ Medium/142. Linked List Cycle II/solution.js
@@ -0,0 +1,24 @@
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function (head) {
+  let fast = head, slow = head;
+
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+
+    if (fast == slow) {
+      slow = head;
+
+      while (fast != slow) {
+        fast = fast.next;
+        slow = slow.next;
+      }
+      return slow;
+    }
+  }
+
+  return null;
+};
