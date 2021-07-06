bec426c3c68df1d673c874b0de0698f894e60a27
Ihor Nikora <ihor.nikora@gmail.com>
2021-07-06 09:36:08 +0300

    566. Reshape the Matrix: JS

--- Easy/566. Reshape the Matrix/solution.js
+++ Easy/566. Reshape the Matrix/solution.js
@@ -6,14 +6,14 @@
  */
 var matrixReshape = function (mat, r, c) {
   const m = mat.length, n = mat[0].length;
+  const size = m * n;
 
-  if (m * n != r * c) return mat;
+  if (size != r * c) return mat;
 
   const res = new Array(r).fill(null).map((i) => new Array(c));
 
-  mat
-    .reduce((acc, cur) => acc.concat(cur), [])
-    .forEach((v, i) => (res[(i / c) >> 0][i % c] = v));
+  for (let i = 0; i < size; i++)
+    res[(i / c) >> 0][i % c] = mat[(i / n) >> 0][i % n];
 
   return res;
 };
