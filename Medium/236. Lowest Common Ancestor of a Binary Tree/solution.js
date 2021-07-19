af84a285fb6b6616ac3a7d0ff7439b2149f10f08
Ihor Nikora <ihor.nikora@gmail.com>
2021-07-19 22:56:41 +0300

    236. Lowest Common Ancestor of a Binary Tree: JS

--- Medium/236. Lowest Common Ancestor of a Binary Tree/solution.js
+++ Medium/236. Lowest Common Ancestor of a Binary Tree/solution.js
@@ -5,14 +5,10 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function (root, p, q) {
-  if (!root) return null;
-  if (root == p || root == q) return root;
+  if (!root || root == p || root == q) return root;
 
   const left = lowestCommonAncestor(root.left, p, q);
   const right = lowestCommonAncestor(root.right, p, q);
 
-  if (left && right) return root;
-  if (left) return left;
-  if (right) return right;
-  return null;
+  return left && right ? root : left || right;
 };
