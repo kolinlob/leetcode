549d488bee00eb940e1fe912863ca7f52befd099
Ihor Nikora <ihor.nikora@gmail.com>
2021-08-23 11:13:44 +0300

    653. Two Sum IV - Input is a BST: JS

--- Easy/653. Two Sum IV - Input is a BST/solution.js
+++ Easy/653. Two Sum IV - Input is a BST/solution.js
@@ -6,21 +6,15 @@
 var findTarget = function (root, k) {
   return traverse(root, k);
 
-  function traverse(node, k) {
+  function traverse(node, v) {
     if (!node) return false;
+    if (v - node.val != node.val && find(root, v - node.val)) return true;
+    return traverse(node.left, v) || traverse(node.right, v);
 
-    if (k - node.val != node.val && find(root, k - node.val)) return true;
-
-    return traverse(node.left, k) || traverse(node.right, k);
-  }
-
-  function find(node, v) {
-    if (!node) return false;
-
-    if (node.val == v) return true;
-
-    if (v < node.val) return find(node.left, v);
-
-    return find(node.right, v);
+    function find(node, v) {
+      if (!node) return false;
+      if (node.val == v) return true;
+      return find(v < node.val ? node.left : node.right, v);
+    }
   }
 };
