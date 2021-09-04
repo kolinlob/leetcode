e961467bdb5a0762b47694a72aad4117cbda6245
Ihor Nikora <ihor.nikora@gmail.com>
2021-09-04 14:30:07 +0300

    876. Middle of the Linked List: JS

---
+++ Easy/876. Middle of the Linked List/solution.js
@@ -0,0 +1,14 @@
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function (head) {
+  let slow = head, fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
+};
