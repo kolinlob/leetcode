ceac853f9f1c9df0ca5ad2be4871be2ffd5f8df4
Ihor Nikora <ihor.nikora@gmail.com>
2021-09-24 18:13:33 +0300

    1137. N-th Tribonacci Number: JS

--- Easy/1137. N-th Tribonacci Number/solution.js
+++ Easy/1137. N-th Tribonacci Number/solution.js
@@ -2,15 +2,19 @@
  * @param {number} n
  * @return {number}
  */
-const map = new Map();
-
 var tribonacci = function (n) {
-  if (n < 1) return 0;
-  if (n < 3) return 1;
+  const map = new Map();
 
-  if (!map.has(n - 1)) map.set(n - 1, tribonacci(n - 1));
-  if (!map.has(n - 2)) map.set(n - 2, tribonacci(n - 2));
-  if (!map.has(n - 3)) map.set(n - 3, tribonacci(n - 3));
+  return memo(n);
 
-  return map.get(n - 1) + map.get(n - 2) + map.get(n - 3);
+  function memo(n) {
+    if (n < 1) return 0;
+    if (n < 3) return 1;
+
+    if (!map.has(n - 1)) map.set(n - 1, memo(n - 1));
+    if (!map.has(n - 2)) map.set(n - 2, memo(n - 2));
+    if (!map.has(n - 3)) map.set(n - 3, memo(n - 3));
+
+    return map.get(n - 1) + map.get(n - 2) + map.get(n - 3);
+  }
 };
