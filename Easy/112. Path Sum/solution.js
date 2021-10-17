3169903429c5bc72aeade716920c1b109ea32f1f
Ihor Nikora <ihor.nikora@gmail.com>
2021-10-17 11:36:37 +0300

    112. Path Sum: JS

---
+++ Easy/112. Path Sum/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function (root, targetSum) {
+  if (root == null) return false;
+  if (root.val == targetSum && root.left == null && root.right == null)
+    return true;
+
+  return (
+    hasPathSum(root.left, targetSum - root.val) ||
+    hasPathSum(root.right, targetSum - root.val)
+  );
+};
