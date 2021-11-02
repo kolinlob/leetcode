c60b1d3b002d25041509f08f176361b93f72707d
Ihor Nikora <ihor.nikora@gmail.com>
2021-11-02 11:04:20 +0200

    904. Fruit Into Baskets: JS

--- Medium/904. Fruit Into Baskets/solution.js
+++ Medium/904. Fruit Into Baskets/solution.js
@@ -2,25 +2,23 @@
  * @param {number[]} fruits
  * @return {number}
  */
-var totalFruit = function (fruits) {
-  let max = 0, sum = 0, i = 0, j = 0, trees = {};
+ var totalFruit = function (fruits) {
+  let max = 0, i = 0, j = 0, trees = {};
 
   while (j < fruits.length) {
     trees[fruits[j]] = trees[fruits[j]] + 1 || 1;
-    sum += 1;
     j++;
 
     while (Object.keys(trees).length > 2) {
-      trees[fruits[i]] -= 1;
-      sum -= 1;
+        trees[fruits[i]] -= 1;
 
       if (trees[fruits[i]] == 0)
         delete trees[fruits[i]];
-
+      
       i++;
     }
 
-    max = Math.max(max, sum);
+    max = Math.max(max, j - i);
   }
 
   return max;
