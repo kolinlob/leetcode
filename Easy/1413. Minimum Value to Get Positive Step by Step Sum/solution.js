34a297608348b028672d482f6bacb760503eadbd
Ihor Nikora <ihor.nikora@gmail.com>
2021-11-11 16:33:34 +0200

    1413. Minimum Value to Get Positive Step by Step Sum: JS

--- Easy/1413. Minimum Value to Get Positive Step by Step Sum/solution.js
+++ Easy/1413. Minimum Value to Get Positive Step by Step Sum/solution.js
@@ -3,11 +3,10 @@
  * @return {number}
  */
 var minStartValue = function (nums) {
-  let start = nums[0] < 1 ? 1 - nums[0] : 1,
-      sum = start;
+  let start = 1, sum = start;
 
-  for (let i = 0; i < nums.length; i++) {
-    sum += nums[i];
+  for (let num of nums) {
+    sum += num;
     if (sum < 1) {
       start += 1 - sum;
       sum = 1;
