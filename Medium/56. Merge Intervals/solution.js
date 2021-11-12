2071d898aac0fe7c8d92e66ab10ba6d1106e4828
Ihor Nikora <ihor.nikora@gmail.com>
2021-11-13 00:38:44 +0200

    56. Merge Intervals: JS

--- Medium/56. Merge Intervals/solution.js
+++ Medium/56. Merge Intervals/solution.js
@@ -3,21 +3,22 @@
  * @return {number[][]}
  */
 var merge = function (intervals) {
+  if (intervals.length < 2) return intervals;
+
   let merged = [];
   intervals.sort((a, b) => a[0] - b[0]);
 
+  let a = intervals.shift();
+
   while (intervals.length > 0) {
-    let a = intervals.shift();
-
-    if (intervals.length > 0) {
-      let b = intervals.shift();
-
-      if (b[0] > a[1]) {
-        intervals.unshift(b);
-        merged.push(a);
-      } else intervals.unshift([a[0], Math.max(a[1], b[1])]);
-    } else merged.push(a);
+    let b = intervals.shift();
+    if (b[0] > a[1]) {
+      merged.push([...a]);
+      a = b;
+    } else a = [a[0], Math.max(a[1], b[1])];
   }
+
+  merged.push(a);
 
   return merged;
 };
