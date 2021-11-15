42b43c5cf0838ff2913c4e65d82eb8df38104db0
Ihor Nikora <ihor.nikora@gmail.com>
2021-11-15 17:25:05 +0200

    287. Find the Duplicate Number: JS

--- Medium/287. Find the Duplicate Number/cyclic-sort-solution.js
+++ Medium/287. Find the Duplicate Number/cyclic-sort-solution.js
@@ -5,15 +5,14 @@
 var findDuplicate = function (nums) {
   for (let i = 0; i < nums.length; i++) {
     let j = nums[i] - 1;
-    while (i < nums.length && nums[i] !== nums[j] && i !== j) {
+    while (nums[i] !== nums[j]) {
       j = nums[i] - 1;
       [nums[i], nums[j]] = [nums[j], nums[i]];
     }
-  }
 
-  for (let i = 0; i < nums.length; i++)
     if (nums[i] !== i + 1)
-        return nums[i];
+      return nums[i];
+  }
 
   return -1;
 };
