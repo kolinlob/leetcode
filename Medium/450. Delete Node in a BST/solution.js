6d31ec8e210955b0d7f218deecec06ab8390d0cd
Ihor Nikora <ihor.nikora@gmail.com>
2021-11-22 23:27:03 +0200

    450. Delete Node in a BST: JS

--- Medium/450. Delete Node in a BST/solution.js
+++ Medium/450. Delete Node in a BST/solution.js
@@ -5,11 +5,12 @@
  */
 var deleteNode = function (root, key) {
   if (root == null) return null;
-  if (root.val > key) root.left = deleteNode(root.left, key);
-  else if (root.val < key) root.right = deleteNode(root.right, key);
+  if (key < root.val) root.left = deleteNode(root.left, key);
+  else if (key > root.val) root.right = deleteNode(root.right, key);
   else {
     if (root.right == null) return root.left;
     if (root.left == null) return root.right;
+
     let tmp = root;
     root = min(tmp.right);
     root.right = delMin(tmp.right);
