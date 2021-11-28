47f4d9350f319fb6ab4ce7ac4942b42be46a56c4
Ihor Nikora <ihor.nikora@gmail.com>
2021-11-28 23:36:10 +0200

    22. Generate Parentheses: JS

--- Medium/22. Generate Parentheses/iterative-solution.js
+++ Medium/22. Generate Parentheses/iterative-solution.js
@@ -3,15 +3,14 @@
  * @return {string[]}
  */
 var generateParenthesis = function (n) {
-  let queue = [{ s: "", o: 0, c: 0 }],
-      result = [];
+  let queue = [{ s: "", o: 0, c: 0 }], result = [];
 
   while (queue.length > 0) {
     let cur = queue.shift();
     if (cur.o == n && cur.c == n) result.push(cur.s);
     else {
-      if (cur.o < n)     queue.push({ s: cur.s + "(", o: cur.o + 1, c: cur.c });
-      if (cur.c < cur.o) queue.push({ s: cur.s + ")", o: cur.o, c: cur.c + 1 });
+      cur.o < n     && queue.push({ s: cur.s + "(", o: cur.o + 1, c: cur.c });
+      cur.c < cur.o && queue.push({ s: cur.s + ")", o: cur.o, c: cur.c + 1 });
     }
   }
 
