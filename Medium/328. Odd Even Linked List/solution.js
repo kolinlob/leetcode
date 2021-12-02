f51ea5e6d726fe255586eca6a7e0ea467ead9098
Ihor Nikora <ihor.nikora@gmail.com>
2021-12-02 13:59:30 +0200

    328. Odd Even Linked List: JS

---
+++ Medium/328. Odd Even Linked List/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function (head) {
+  if (head == null) return null;
+
+  let odd = head, even = head.next, evenHead = even;
+
+  while (even && even.next) {
+    odd.next = even.next;
+    odd = odd.next;
+    even.next = odd.next;
+    even = even.next;
+  }
+
+  odd.next = evenHead;
+  return head;
+};
