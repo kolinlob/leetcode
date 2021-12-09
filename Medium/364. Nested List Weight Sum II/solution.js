5832058320e4f4227f708f5f02900e9684efcd68
Ihor Nikora <ihor.nikora@gmail.com>
2021-12-09 23:25:31 +0200

    364. Nested List Weight Sum II: JS

---
+++ Medium/364. Nested List Weight Sum II/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {NestedInteger[]} nestedList
+ * @return {number}
+ */
+var depthSumInverse = function (nestedList) {
+  let max = -Infinity, pairs = flat(nestedList, 1);
+  return pairs.reduce((s, [n, d]) => s + (max - d + 1) * n, 0);
+
+  function flat(nested, depth) {
+    let res = [];
+    for (let item of nested) {
+      if (item.isInteger()) {
+        res.push([item.getInteger(), depth]);
+        max = Math.max(max, depth);
+      } else res.push(...flat(item.getList(), depth + 1));
+    }
+    return res;
+  }
+};
