301131cfbae577ef2dd39345d0bc963be7a27b11
Ihor Nikora <ihor.nikora@gmail.com>
2021-12-14 11:21:08 +0200

    938. Range Sum of BST: JS

---
+++ Easy/938. Range Sum of BST/solution.js
@@ -0,0 +1,12 @@
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBST = function (root, low, high) {
+  if (root == null) return 0;
+  if (root.val < low) return rangeSumBST(root.right, low, high);
+  if (root.val > high) return rangeSumBST(root.left, low, high);
+  return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+};
