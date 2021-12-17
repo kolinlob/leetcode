deb4a407d044d312f90f4d52ff874aa81c7c02b2
Ihor Nikora <ihor.nikora@gmail.com>
2021-12-17 13:38:59 +0200

    221. Maximal Square: JS

--- Medium/221. Maximal Square/solution.js
+++ Medium/221. Maximal Square/solution.js
@@ -4,26 +4,16 @@
  */
 var maximalSquare = function (matrix) {
   const m = matrix.length,
-    n = matrix[0].length,
-    dp = new Array(m).fill(null).map(() => new Array(n).fill(0));
+        n = matrix[0].length,
+        dp = new Array(m + 1).fill(null).map(() => new Array(n + 1).fill(0));
 
   let max = 0;
-  for (let i = 0; i < m; ++i) {
-    for (let j = 0; j < n; ++j) {
-      if (matrix[i][j] == "1") {
-        dp[i][j] = 1;
-        if (
-          i > 0 &&
-          j > 0 &&
-          matrix[i - 1][j - 1] == "1" &&
-          matrix[i][j - 1] == "1" &&
-          matrix[i - 1][j] == "1"
-        )
-          dp[i][j] = (1 + Math.sqrt(Math.min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]))) ** 2;
+  for (let i = 1; i < m + 1; ++i)
+    for (let j = 1; j < n + 1; ++j)
+      if (matrix[i - 1][j - 1] == "1") {
+        dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]);
         max = Math.max(max, dp[i][j]);
       }
-    }
-  }
 
-  return max;
+  return max ** 2;
 };
