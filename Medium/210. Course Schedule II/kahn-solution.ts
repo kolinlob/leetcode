75f24eafa3de717deedb14cd2439cae84ac09ae9
Ihor Nikora <ihor.nikora@gmail.com>
2021-12-24 18:03:28 +0200

    210. Course Schedule II: TS

--- Medium/210. Course Schedule II/kahn-solution.ts
+++ Medium/210. Course Schedule II/kahn-solution.ts
@@ -1,26 +1,24 @@
 function findOrder(numCourses: number, prerequisites: number[][]): number[] {
-  let order: number[] = [];
-  let queue: number[] = [];
-  let graph = new Map<number, number[]>();
-  let indegree = new Array(numCourses).fill(0);
+  let order: number[] = [],
+      queue: number[] = [],
+      graph = new Map<number, number[]>(),
+      indgr = new Array(numCourses).fill(0);
 
   for (let [to, from] of prerequisites) {
     if (graph.has(from)) graph.get(from).push(to);
     else graph.set(from, [to]);
-    indegree[to]++;
+    indgr[to]++;
   }
 
-  for (let i = 0; i < indegree.length; ++i)
-    if (indegree[i] == 0) queue.push(i);
+  for (let i = 0; i < indgr.length; ++i)
+    indgr[i] == 0 && queue.push(i);
 
   while (queue.length > 0) {
     let v = queue.shift();
 
     if (graph.has(v))
-      for (let e of graph.get(v)) {
-        indegree[e]--;
-        if (indegree[e] == 0) queue.push(e);
-      }
+      for (let e of graph.get(v))
+        --indgr[e] == 0 && queue.push(e);
 
     order.push(v);
   }
