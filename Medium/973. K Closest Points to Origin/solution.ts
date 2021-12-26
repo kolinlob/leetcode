7c8977e2614942f78e16f029d83082883cc60d9c
Ihor Nikora <ihor.nikora@gmail.com>
2021-12-26 23:06:05 +0200

    973. K Closest Points to Origin: TS

--- Medium/973. K Closest Points to Origin/solution.ts
+++ Medium/973. K Closest Points to Origin/solution.ts
@@ -1,18 +1,10 @@
 function kClosest(points: number[][], k: number): number[][] {
-  let maxheap = new MaxPriorityQueue({
-    priority: (a: { d: number; i: number }) => a.d,
-  });
+  let maxheap = new MaxPriorityQueue();
 
-  for (let i = 0; i < points.length; ++i) {
-    let [x, y] = points[i];
-    maxheap.enqueue({ d: x * x + y * y, i });
-
+  for (let [x, y] of points) {
+    maxheap.enqueue([x, y], x * x + y * y);
     if (maxheap.size() > k) maxheap.dequeue();
   }
 
-  let res = [];
-  while (!maxheap.isEmpty())
-    res.push(points[maxheap.dequeue().element.i]);
-
-  return res;
+  return maxheap.toArray().map((x) => x.element);
 }
