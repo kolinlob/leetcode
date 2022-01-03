c985dc2f963776ee7b788278edba6a440ce674b4
Ihor Nikora <ihor.nikora@gmail.com>
2022-01-03 10:24:14 +0200

    997. Find the Town Judge: TS

---
+++ Easy/997. Find the Town Judge/solution.ts
@@ -0,0 +1,17 @@
+function findJudge(n: number, trust: number[][]): number {
+  let grapgh = new Map<number, any>();
+
+  for (let i = 1; i <= n; ++i)
+    grapgh.set(i, { i: [], o: [] });
+
+  for (let [person, trustee] of trust) {
+    grapgh.get(person).o.push(trustee);
+    grapgh.get(trustee).i.push(person);
+  }
+
+  for (let [k, v] of grapgh.entries())
+    if (v.o.length == 0 && v.i.length == n - 1 && v.i.indexOf(k) == -1)
+      return k;
+
+  return -1;
+}
