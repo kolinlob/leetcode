8c3b20117b2c07abcd35d9740cbc418c140b198d
Ihor Nikora <ihor.nikora@gmail.com>
2022-01-03 11:00:48 +0200

    997. Find the Town Judge: TS

--- Easy/997. Find the Town Judge/array-solution.ts
+++ Easy/997. Find the Town Judge/array-solution.ts
@@ -1,14 +1,13 @@
 function findJudge(n: number, trust: number[][]): number {
-  let g = new Array(n).fill(null).map(() => ({ indegree: 0, outdegree: 0 }));
+  let g = new Array(n + 1).fill(null).map(() => ({ indegree: 0, outdegree: 0 }));
 
-  for (let i = 0; i < trust.length; ++i) {
-    let [person, trustee] = trust[i];
-    g[person - 1].outdegree++;
-    g[trustee - 1].indegree++;
+  for (let [person, trustee] of trust) {
+    g[person].outdegree++;
+    g[trustee].indegree++;
   }
 
-  for (let [i, { indegree, outdegree }] of g.entries())
-    if (outdegree == 0 && indegree == n - 1) return i + 1;
+  for (let i = 1; i <= n; ++i)
+    if (g[i].outdegree == 0 && g[i].indegree == n - 1) return i;
 
   return -1;
 }
