36d95fc245da48b068f26ec86d27f18d340775a9
Ihor Nikora <ihor.nikora@gmail.com>
2022-01-11 11:46:53 +0200

    1022. Sum of Root To Leaf Binary Numbers: TS

--- Easy/1022. Sum of Root To Leaf Binary Numbers/solution.ts
+++ Easy/1022. Sum of Root To Leaf Binary Numbers/solution.ts
@@ -21,3 +21,47 @@ function sumRootToLeaf(root: TreeNode | null): number {
     dfs(node.right, path);
   }
 }
+
+function sumRootToLeaf(root: TreeNode | null): number {
+  let paths: number[] = [];
+
+  dfs(root, 0);
+
+  return paths.reduce((acc, cur) => acc + cur, 0);
+
+  function dfs(node: TreeNode | null, path: number): void {
+    if (node == null) return;
+
+    path = (path << 1) | node.val;
+
+    if (node.left == null && node.right == null) {
+      paths.push(path);
+      return;
+    }
+
+    dfs(node.left, path);
+    dfs(node.right, path);
+  }
+}
+
+function sumRootToLeaf(root: TreeNode | null): number {
+  let sum = 0;
+
+  dfs(root, 0);
+
+  return sum;
+
+  function dfs(node: TreeNode | null, path: number): void {
+    if (node == null) return;
+
+    path = (path << 1) | node.val;
+
+    if (node.left == null && node.right == null) {
+      sum += path;
+      return;
+    }
+
+    dfs(node.left, path);
+    dfs(node.right, path);
+  }
+}
