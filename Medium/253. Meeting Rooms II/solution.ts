2cdd2aceafcd056646a23e6c30c7c5500737b0c4
Ihor Nikora <ihor.nikora@gmail.com>
2022-01-12 13:00:14 +0200

    253. Meeting Rooms II: TS

--- Medium/253. Meeting Rooms II/solution.ts
+++ Medium/253. Meeting Rooms II/solution.ts
@@ -1,14 +1,14 @@
 function minMeetingRooms(intervals: number[][]): number {
-  let ends = new MinPriorityQueue({ priority: (a) => a[1] });
+  let ongoing = new MinPriorityQueue({ priority: (a) => a[1] });
   let rooms = 0;
 
   intervals.sort((a, b) => a[0] - b[0]);
 
   for (let meeting of intervals) {
-    if (ends.isEmpty() || meeting[0] < ends.front().element[1]) rooms += 1;
-    else ends.dequeue();
+    if (ongoing.isEmpty() || meeting[0] < ongoing.front().element[1]) rooms += 1;
+    else ongoing.dequeue();
 
-    ends.enqueue(meeting);
+    ongoing.enqueue(meeting);
   }
 
   return rooms;
