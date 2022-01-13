1fc05f7e6ba5371f1c2974fc61f5463878eb6f36
Ihor Nikora <ihor.nikora@gmail.com>
2022-01-13 11:07:47 +0200

    452. Minimum Number of Arrows to Burst Balloons: TS

---
+++ Medium/452. Minimum Number of Arrows to Burst Balloons/solution.ts
@@ -0,0 +1,16 @@
+function findMinArrowShots(points: number[][]): number {
+  let minArrows = points.length;
+
+  points.sort((a, b) => a[0] - b[0]);
+
+  let x = [-Infinity, -Infinity];
+
+  for (let balloon of points) {
+    if (balloon[0] <= x[1] && x[0] <= balloon[1]) {
+      x = [Math.max(x[0], balloon[0]), Math.min(x[1], balloon[1])];
+      minArrows -= 1;
+    } else x = balloon;
+  }
+
+  return minArrows;
+}
