c0a4c8d6439a5cf92ff84529e0ded5c37258e3f6
Ihor Nikora <ihor.nikora@gmail.com>
2022-01-14 10:07:30 +0200

    8. String to Integer (atoi): TS

---
+++ Medium/8. String to Integer (atoi)/solution.ts
@@ -0,0 +1,21 @@
+function myAtoi(s: string): number {
+  if (!s) return 0;
+
+  let split = s.trim().split(/(\d+)/);
+  if (split.length <= 1 || split[0].length > 1) return 0;
+
+  let sign = null;
+  switch (split[0]) {
+    case "":
+    case "+": { sign = 1; break; }
+    case "-": { sign = -1; break; }
+    default: return 0;
+  }
+
+  let integer = Number.parseInt(split[1]);
+
+  if (sign < 0 && integer > Math.pow(2, 31)) return Math.pow(-2, 31);
+  if (sign > 0 && integer > Math.pow(2, 31) - 1) return Math.pow(2, 31) - 1;
+
+  return sign * integer;
+}
