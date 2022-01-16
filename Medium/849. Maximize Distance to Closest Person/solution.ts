11f914ac01c1b95e59dec24083093194954d08cd
Ihor Nikora <ihor.nikora@gmail.com>
2022-01-16 13:23:24 +0200

    849. Maximize Distance to Closest Person: TS

---
+++ Medium/849. Maximize Distance to Closest Person/solution.ts
@@ -0,0 +1,16 @@
+function maxDistToClosest(seats: number[]): number {
+  let left = 0;
+  while (seats[left] == 0) left++;
+
+  let right = 0;
+  while (seats[seats.length - 1 - right] == 0) right++;
+
+  let mid = 0, cur = 0;
+  for (let i = left; i < seats.length - right; ++i) {
+    if (seats[i] == 1) cur = 0;
+    else cur++;
+    mid = Math.max(mid, Math.ceil(cur / 2));
+  }
+
+  return Math.max(left, mid, right);
+}
