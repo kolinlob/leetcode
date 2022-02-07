7bfc94031fcc8ed25e754a0ba3349f6dd272224b
Ihor Nikora <ihor.nikora@gmail.com>
2022-02-07 11:36:57 +0200

    389. Find the Difference: TS

--- 1. Easy/389. Find the Difference/solution.ts
+++ 1. Easy/389. Find the Difference/solution.ts
@@ -1,12 +1,12 @@
 function findTheDifference(s: string, t: string): string {
   const sMap = [...s].reduce((m, c) => { m[c] = 1 + (m[c] || 0); return m }, {});
-  const tMap = [...t].reduce((m, c) => { m[c] = 1 + (m[c] || 0); return m }, {});
 
-  for (let tkey in tMap) {
-    if (tkey in sMap) {
-      if (sMap[tkey] < tMap[tkey]) return tkey;
-    } else return tkey;
-  }
+  for (let ch of t) {
+    if (ch in sMap) {
+        if(--sMap[ch] < 0) return ch;
+    }
+    else return ch;
+}
 
-  return "";
+return "";
 }
