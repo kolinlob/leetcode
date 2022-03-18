babc27065d494c066d7153841584d28e208ce075
Ihor Nikora <ihor.nikora@gmail.com>
2022-03-18 12:41:06 +0200

    316. Remove Duplicate Letters: TS

---
+++ 2. Medium/316. Remove Duplicate Letters/solution.ts
@@ -0,0 +1,23 @@
+function removeDuplicateLetters(s: string): string {
+  const stack = [];
+  const used = new Set<string>();
+
+  const map = new Map<string, number>();
+  for (let i = 0; i < s.length; ++i) map.set(s[i], i);
+
+  for (let i = 0; i < s.length; ++i) {
+    if (used.has(s[i])) continue;
+
+    while (
+      stack.length > 0 &&
+      stack[stack.length - 1] > s[i] &&
+      map.get(stack[stack.length - 1]) > i
+    )
+      used.delete(stack.pop());
+
+    used.add(s[i]);
+    stack.push(s[i]);
+  }
+
+  return stack.join("");
+}
